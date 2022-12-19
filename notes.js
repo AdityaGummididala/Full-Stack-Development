2ea19d1f1bd20e55a34fee5e5f6e67eaaa1f579a
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-19 20:49:32 +0530

    imp

--- notes.js
+++ notes.js
@@ -136,3 +136,34 @@ console.log(maximum)
 console.log(minimum)
 console.log(a)
 */
+//hypotenuse calc program
+/*
+let a;
+let b;
+let c;
+
+a= window.prompt("enter side A")
+a=Number(a)
+
+b=window.prompt("enter the side B")
+b=Number(b)
+
+c = Math.pow(a,2)+ (Math.pow(b,2));
+c=Math.sqrt(c);
+console.log(c)
+*/
+//or
+/*
+let a;
+let b;
+let c;
+document.getElementById("mybutton").onclick = function()
+{ 
+    a=document.getElementById("atext").value;
+    a=Number(a);
+    b=document.getElementById("btext").value;
+    b=Number(b);
+    c = Math.sqrt(Math.pow(a,2)+ Math.pow(b,2));
+    document.getElementById("Clabel").innerHTML="Side C :" + c ;
+}
+*/
