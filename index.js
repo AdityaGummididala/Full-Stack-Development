975400b31b901c7eef8260c5980b1f82f464f9a7
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-19 10:48:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("Not again");
 console.log('Hello World')    //we can print a statement either using "" or '' (double quotes or single quotes)
 window.alert("bruh hello world again?") //this pops an alert message in the website
+/*
+this 
+is 
+a multiline comment
+*/
