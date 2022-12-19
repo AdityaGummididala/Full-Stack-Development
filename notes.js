b1e7368f422340b8b1236fceee01ef45baa29827
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-19 19:42:58 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -91,4 +91,14 @@ age = Number(age);
 age +=1;
 console.log(typeof age)
 console.log("happy birthday! You are", age ," years old");
+--
+let x;
+let y;
+let z;
+x= Number("3.14");
+y= String(3.14);
+z= Boolean("");
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
 */
