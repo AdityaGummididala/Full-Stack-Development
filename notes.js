bb785dfc9f54ad4e527d19b63fa46ff2b083969b
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-19 18:42:07 +0530

    main

---
+++ notes.js
@@ -0,0 +1,60 @@
+//console.log("Not again");
+//console.log('Hello World')    //we can print a statement either using "" or '' (double quotes or single quotes)
+//window.alert("bruh hello world again?") //this pops an alert message in the website
+/*
+this 
+is 
+a multiline comment
+*/
+//variables
+/*
+->A variable is a container for storing data
+->A variable behaves as if it was the value that it contains
+->There are two steps:-
+1.Declaration(var,let,const)
+2.Assignment(= assignment operator)
+*/
+/*
+let age;
+age=18;
+age= age+1;
+console.log(age);
+let firstname = "Aditya Gummididala"; //string
+console.log(firstname);
+let student = true //boolean it is one of two values true of false
+console.log(student);
+console.log("hello",firstname);
+console.log("you are ",age,"years old");
+console.log(student,"you are enrolled");
+document.getElementById("p1").innerHTML = "Hello "+firstname;
+document.getElementById("p2").innerHTML = "you are "+age+" years old";
+document.getElementById("p3").innerHTML = "Enrolled: "+student;
+*/
+/*-----------------------------------------------------------------------*/
+/* arthimetic expression is a combination of..
+operands(values,variables,etc.)
+operators(+ - * / %)
+that can be evaluated to a value
+ex:- y=x+5;
+*/
+//let students = 20;
+//nostudents = students-1;
+//students = students + 1;
+//console.log(students);
+//console.log(nostudents);
+//extra = students % 3;
+//console.log(extra);
+//students += 1; //(this is called augumented assignment operator i.e shortcut for students = students +1;)
+//students -= 2;
+//students *= 3;
+//students /=2;
+//console.log(students);
+/*
+operator precedence
+1.parenthesis()
+2.exponents
+3.multiplication&division
+4.addition & subtraction
+*/
+//let result = 2+2*(3+4);
+//console.log(result)
