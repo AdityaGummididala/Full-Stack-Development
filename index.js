bb35ad5deda50cdc24b4a4d441f222380622b54d
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-19 18:04:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-console.log("Not again");
-console.log('Hello World')    //we can print a statement either using "" or '' (double quotes or single quotes)
-window.alert("bruh hello world again?") //this pops an alert message in the website
+//console.log("Not again");
+//console.log('Hello World')    //we can print a statement either using "" or '' (double quotes or single quotes)
+//window.alert("bruh hello world again?") //this pops an alert message in the website
 /*
 this 
 is 
 a multiline comment
+*/
+//variables
+/*
+->A variable is a container for storing data
+->A variable behaves as if it was the value that it contains
+->There are two steps:-
+1.Declaration(var,let,const)
+2.Assignment(= assignment operator)
 */
