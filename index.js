03b66ef701127e06d73a3a02d04ad2074bd963ec
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-19 20:46:51 +0530

    imp

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 let a;
 let b;
 let c;
@@ -11,3 +12,16 @@ b=Number(b)
 c = Math.pow(a,2)+ (Math.pow(b,2));
 c=Math.sqrt(c);
 console.log(c)
+*/
+let a;
+let b;
+let c;
+document.getElementById("mybutton").onclick = function()
+{ 
+    a=document.getElementById("atext").value;
+    a=Number(a);
+    b=document.getElementById("btext").value;
+    b=Number(b);
+    c = Math.sqrt(Math.pow(a,2)+ Math.pow(b,2));
+    document.getElementById("Clabel").innerHTML="Side C :" + c ;
+}
