e8ec86fdca93449c1a8478da87db392420084ad6
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-21 10:16:57 +0530

    upd

--- notes.js
+++ notes.js
@@ -225,3 +225,22 @@ document.getElementById("reset").onclick=function() {
 let x = Math.floor(((Math.random() *6)+1))
 console.log(x)
 */
+//useful string properties and methods
+/*
+let username="    adityaaaaaaaaaaaaaaaaaaaaaaaaaa   "
+//console.log(username.length)
+//let x = username.charAt(2)
+////console.log("the char at 2nd index is ",x)
+//console.log(username.indexOf("y"))
+//console.log(username.lastIndexOf("a"))
+username = username.trim()
+console.log(username)
+username=username.toUpperCase();
+y=username.toLowerCase();
+console.log(username)
+console.log(y)
+let phone;
+phone="12-34-56-79-0"
+phone=phone.replaceAll("-" , "")
+console.log(phone)
+*/
