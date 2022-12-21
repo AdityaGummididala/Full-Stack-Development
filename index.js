b0d0d074c50775b2124db4665f2c26453300d026
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-21 10:10:35 +0530

    upd

---
+++ index.js
@@ -0,0 +1,7 @@
+//useful string properties and methods
+let username="adityaaaaaaaaaaaaaaaaaaaaaaaaaa"
+console.log(username.length)
+let x = username.charAt(2)
+console.log("the char at 2nd index is ",x)
+console.log(username.indexOf("y"))
+console.log(username.lastIndexOf("a"))
